fc550e1df6eadc4760d0e1f1ef017a74842d956d
h-tate-mitchell <htatemitchell@gmail.com>
2025-11-09 19:37:20 -0700

    added forest footer transition hero, fixed accolade overflow

--- src/components/ImageFootTransition.tsx
+++ src/components/ImageFootTransition.tsx
@@ -3,7 +3,17 @@ import styles from '@/styles/ImageFootTransition.module.css'
 
 const ImageFootTransition = () => {
   return (
-    <div className={styles.imageTransition}></div>
+    <div 
+      className={styles.imageTransition} 
+      style={{
+        backgroundImage: `url('/assets/photos/forest-2.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%',
+      }}
+    >
+    </div>
   )
 }
 
